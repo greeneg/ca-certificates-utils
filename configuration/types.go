8699ef920a49a14e184230cfa3880dea48485fdf
Gary Greene <greeneg@tolharadys.net>
2026-05-31 14:31:34 -0400

    Expand configuration type and functions
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- configuration/types.go
+++ configuration/types.go
@@ -3,24 +3,47 @@ package configuration
 import (
 	"encoding/json"
 	"fmt"
+	"os/exec"
 )
 
 type Configuration struct {
-	StateDir           string   `json:"stateDir"`
-	HooksDirList       []string `json:"pluginDirectories"`
-	Verbose            bool     `json:"verbose"`
-	DestDir            string   `json:"rootDir"`
-	Fresh              bool     `json:"clean"`
-	UseSyslog          bool     `json:"useSyslog"`
-	UseLogFile         bool     `json:"useLogFile"`
-	UseConsoleLog      bool     `json:"useConsoleLog"`
-	LogFile            string   `json:"logFile"`
-	SyslogFacility     string   `json:"syslogFacility"`
-	DefaultSyslogLevel string   `json:"defaultSyslogLevel"`
+	StateDir            string   `json:"stateDir"`
+	HooksDirList        []string `json:"pluginDirectories"`
+	Verbose             bool     `json:"verbose"`
+	DestDir             string   `json:"rootDir"`
+	Fresh               bool     `json:"clean"`
+	UpdateCertDataFile  bool     `json:"updateCertificateDataFile"`
+	UseSyslog           bool     `json:"useSyslog"`
+	UseLogFile          bool     `json:"useLogFile"`
+	UseConsoleLog       bool     `json:"useConsoleLog"`
+	LogFile             string   `json:"logFile"`
+	EnableNssDbPlugin   bool     `json:"enableNssDbPlugin"`
+	EnableJavaP12Plugin bool     `json:"enableJavaP12Plugin"`
+	SyslogFacility      string   `json:"syslogFacility"`
+	DefaultSyslogLevel  string   `json:"defaultSyslogLevel"`
 }
 
 func NewConfiguration() Configuration {
 	c := Configuration{}
+
+	// find certutil and keytool. If not found, disable NssDb and Java p12 plugins, respectively
+	certutilPath, err := exec.LookPath("certutil")
+	if err != nil {
+		fmt.Printf("certutil not found, disabling NSS DB plugin: %v\n", err)
+		c.EnableNssDbPlugin = false
+	} else {
+		fmt.Printf("certutil found at: %s\n", certutilPath)
+		c.EnableNssDbPlugin = true
+	}
+
+	keytoolPath, err := exec.LookPath("keytool")
+	if err != nil {
+		fmt.Printf("keytool not found, disabling Java p12 plugin: %v\n", err)
+		c.EnableJavaP12Plugin = false
+	} else {
+		fmt.Printf("keytool found at: %s\n", keytoolPath)
+		c.EnableJavaP12Plugin = true
+	}
 
 	c.StateDir = "var/lib/ca-certificates"
 	c.HooksDirList = append(c.HooksDirList, "etc/ca-certificates/update.d")
@@ -29,6 +52,7 @@ func NewConfiguration() Configuration {
 	c.DestDir = "/"
 	c.Fresh = false
 	c.LogFile = "/var/log/update-ca.log"
+	c.UpdateCertDataFile = false
 	c.UseSyslog = true
 	c.UseLogFile = false
 	c.UseConsoleLog = true
